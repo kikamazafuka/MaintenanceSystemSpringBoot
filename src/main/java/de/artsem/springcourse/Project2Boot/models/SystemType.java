606c130ddbbbcc4dd7c82f27d7f7c86626609502
Artur Semenas <uncelfuka@gmail.com>
2023-08-19 23:00:03 +0200

    SystemType creation

---
+++ src/main/java/de/artsem/springcourse/Project2Boot/models/SystemType.java
@@ -0,0 +1,28 @@
+package de.artsem.springcourse.Project2Boot.models;
+
+public class SystemType {
+
+    private int id;
+    private String name;
+
+    public SystemType(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
