f68727dbb592720c0ab0b8c82f8fd906df8053da
Artur Semenas <uncelfuka@gmail.com>
2023-08-28 22:43:43 +0200

    SystemType Controller add newSystemType get method

--- src/main/java/de/artsem/springcourse/Project2Boot/controllers/SystemTypesController.java
+++ src/main/java/de/artsem/springcourse/Project2Boot/controllers/SystemTypesController.java
@@ -5,6 +5,9 @@ import de.artsem.springcourse.Project2Boot.models.SystemType;
 import de.artsem.springcourse.Project2Boot.services.SystemTypesService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,11 +24,21 @@ public class SystemTypesController {
     }
 
 
-    @RequestMapping
+    @GetMapping
     private String index (Model model){
         List<SystemType> systemTypeList = systemTypesService.findAll();
         model.addAttribute("systemTypes", systemTypeList);
         return "systemTypes/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show (@PathVariable ("id") int id){
+        return "systemTypes/index";
+    }
+
+    @GetMapping("/new")
+    public String newSystemType (@ModelAttribute ("systemType") SystemType systemType){
+        return "systemTypes/new";
     }
 
 }
