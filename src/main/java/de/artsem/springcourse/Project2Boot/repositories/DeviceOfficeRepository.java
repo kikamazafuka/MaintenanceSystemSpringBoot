7eda0ba1875e262db5fda34e88f6acc155823666
Artur Semenas <uncelfuka@gmail.com>
2023-09-14 22:16:02 +0200

    DeviceOfficeRepository  findByOfficeAndSystemType

--- src/main/java/de/artsem/springcourse/Project2Boot/repositories/DeviceOfficeRepository.java
+++ src/main/java/de/artsem/springcourse/Project2Boot/repositories/DeviceOfficeRepository.java
@@ -3,11 +3,16 @@ package de.artsem.springcourse.Project2Boot.repositories;
 import de.artsem.springcourse.Project2Boot.models.Device;
 import de.artsem.springcourse.Project2Boot.models.DeviceOffice;
 import de.artsem.springcourse.Project2Boot.models.Office;
+import de.artsem.springcourse.Project2Boot.models.SystemType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DeviceOfficeRepository extends JpaRepository<DeviceOffice, Integer> {
 
     DeviceOffice findByOfficeAndDevice(Office office, Device device);
+
+    List<DeviceOffice> findByOfficeAndSystemType(Office office, SystemType systemType);
 }
