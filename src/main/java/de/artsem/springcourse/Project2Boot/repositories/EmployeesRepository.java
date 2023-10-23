d2805d0c371d6b0943ac20079c67f4c554cbdab0
Artur Semenas <uncelfuka@gmail.com>
2023-10-23 22:48:09 +0200

    EmployeesRepository findAllByNameStartingWith()

--- src/main/java/de/artsem/springcourse/Project2Boot/repositories/EmployeesRepository.java
+++ src/main/java/de/artsem/springcourse/Project2Boot/repositories/EmployeesRepository.java
@@ -4,7 +4,10 @@ import de.artsem.springcourse.Project2Boot.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeesRepository extends JpaRepository<Employee, Integer> {
 
+    List<Employee> findAllByNameStartingWith(String startingChar);
 }
